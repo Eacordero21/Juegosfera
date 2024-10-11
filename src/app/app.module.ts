ae4ed0a7ca11e72fde93e93b0663f0bc025dcb21
azulluna420 <azullunahedz@gmail.com>
2024-10-11 07:36:26 -0600

    emilio pendejo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
-<<<<<<< HEAD
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { TiendaComponent } from './tienda/tienda.component';
-import { MainComponent } from './main/main.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    TiendaComponent,
-    MainComponent
-  ],
-=======
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
@@ -30,7 +14,6 @@ import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
->>>>>>> 35b3c02bc67fc7849e6ef85f9ac2147febdda389
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  currentSlide: number = 0;
+  slides: number[] = [0, 1, 2];
 
+  nextSlide(): void {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  prevSlide(): void {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+  goToSlide(index: number): void {
+    this.currentSlide = index;
+  }
 }
+
